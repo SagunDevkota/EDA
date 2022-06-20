35b5c494e2fb97caff2edca8c26421a630df978b
SagunDevkota <sagundevyt@gmail.com>
2022-06-20 17:30:11 +0545

    Configured temp user{will be removed in future} for authentication

--- src/main/java/eda/config/SecurityConfig.java
+++ src/main/java/eda/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package eda.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -7,6 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //to create the spring security filter chain
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("sagun")
+		.password("{bcrypt}$2a$12$UL.HyGUxf4aSTD6nyf4c2uMBmn72z1zWo6w01OgCK7xJEaSimpoe2")
+		.roles("admin");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.
