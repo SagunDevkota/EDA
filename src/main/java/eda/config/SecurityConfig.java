ac47d92c50f0abc3572d87eee47e6685efce3eba
SagunDevkota <sagundevyt@gmail.com>
2022-06-20 17:16:58 +0545

    Default formlogin and httpBasic configured along with .gitignore file

--- src/main/java/eda/config/SecurityConfig.java
+++ src/main/java/eda/config/SecurityConfig.java
@@ -1,9 +1,21 @@
 package eda.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //to create the spring security filter chain
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+		authorizeRequests().
+		anyRequest().
+		authenticated().
+		and().
+		formLogin().
+		and().
+		httpBasic();
+	}
 }
