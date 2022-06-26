93423c76e463662ef6699ab5a44d4c658b7bd06a
SagunDevkota <sagundevyt@gmail.com>
2022-06-26 16:37:19 +0545

    CommonsMultipartResolver Bean created

--- src/main/java/eda/config/AppConfig.java
+++ src/main/java/eda/config/AppConfig.java
@@ -5,11 +5,16 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.MultipartFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -24,7 +29,7 @@ public class AppConfig {
 		internalResourceViewResolver.setSuffix(".jsp");
 		return internalResourceViewResolver;
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		// to store and verify encoded password
@@ -54,5 +59,13 @@ public class AppConfig {
 		// Not used currently
 		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource());
 		return jdbcUserDetailsManager;
+	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+	    commonsMultipartResolver.setMaxUploadSize(1073741824);//1GB
+	    commonsMultipartResolver.setDefaultEncoding("UTF-8");
+	    return commonsMultipartResolver;
 	}
 }
