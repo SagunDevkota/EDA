a60e860742fa215a0aeb04c0bf66338898bbb3cb
Abhishek <abhishek.adhikari276@gmail.com>
2022-06-27 22:25:05 +0545

    refine and validate form

--- src/main/webapp/WEB-INF/resources/js/script.js
+++ src/main/webapp/WEB-INF/resources/js/script.js
@@ -3,3 +3,19 @@ document.querySelector('.img-btn').addEventListener('click', function()
 		document.querySelector('.cont').classList.toggle('s-signup')
 	}
 );
+
+function validateForm() {
+	var em = document.getElementById("email").value;
+	var pw = document.getElementById("password").value;
+	var regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+	if(em == "") {
+		document.getElementById("emError").innerHTML = "email is required";
+	} else if(!regex.test(em)) {
+		document.getElementById("emError").innerHTML = "email format is incorrect";
+	}
+	if(pw == "") {
+		document.getElementById("pwError").innerHTML = "password is required";
+	}
+}
+
