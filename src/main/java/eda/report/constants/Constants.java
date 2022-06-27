005b4fd29f7937241645318bc6be0f8ca5aa5c42
SagunDevkota <sagundevyt@gmail.com>
2022-06-27 16:59:44 +0545

    Constants of column types and identifiers

---
+++ src/main/java/eda/report/constants/Constants.java
@@ -0,0 +1,19 @@
+package eda.report.constants;
+
+/**
+ * 
+ * @author Sagun Devkota
+ * Column types and respective identifier for report generation.
+ *
+ */
+public class Constants {
+	public static final String NUMERIC = "Numeric";
+	public static final int NUMERIC_IND = 0;
+	public static final String CATEGORICAL = "Categorical";
+	public static final int CATEGORICAL_IND = 1;
+	public static final String BOOLEAN = "Boolean";
+	public static final int BOOLEAN_IND = 2;
+	public static final int NO_OF_CATEGORY = 3;
+	public static final String TRUE_LITERAL = "true";
+	public static final String FALSE_LITERAL = "false";
+}
