a869db622398ab392dc531e984964a7086eb343c
SagunDevkota <sagundevyt@gmail.com>
2022-07-03 12:21:07 +0545

    onSubmit method corrected with return statements and removal of onclick event which was unnecessary

--- src/main/webapp/WEB-INF/resources/js/script.js
+++ src/main/webapp/WEB-INF/resources/js/script.js
@@ -4,28 +4,30 @@ document.querySelector('.img-btn').addEventListener('click', function()
 	}
 );
 
-document.getElementById("sInButton").addEventListener('click', validateSinForm);
-document.getElementById("sUpButton").addEventListener('click', validateSupForm);
+//document.getElementById("sInButton").addEventListener('click', validateSinForm);
+//document.getElementById("sUpButton").addEventListener('click', validateSupForm);
 
 
-function validateSinForm(event) {
-	event.preventDefault();
+function validateSinForm() {
 	var em = document.getElementById("email").value;
 	var pw = document.getElementById("password").value;
 	var regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
 	if(em == "") {
 		document.getElementById("emError").innerHTML = "email is required";
+		return false;
 	} else if(!regex.test(em)) {
 		document.getElementById("emError").innerHTML = "email format is incorrect";
+		return false;
 	}
 	if(pw == "") {
 		document.getElementById("pwError").innerHTML = "password is required";
+		return false;
 	}
+	return true;
 };
 
-function validateSupForm(event) {
-	event.preventDefault();
+function validateSupForm() {
 	var name = document.getElementById("sUpName").value;
 	var email = document.getElementById("sUpEmail").value;
 	var psw = document.getElementById("sUpPassword").value;
@@ -35,21 +37,24 @@ function validateSupForm(event) {
 
 	if(email == "") {
 		document.getElementById("sUpEmError").innerHTML = "email is required";
+		return false;
 	} else if(!regex.test(email)) {
 		document.getElementById("sUpEmError").innerHTML = "email format is incorrect";
+		return false;
 	}
 	if(name == "") {
 		document.getElementById("sUpNameError").innerHTML = "name is required";
+		return false;
 	}
 	if(psw == "") {
 		document.getElementById("sUpPwError").innerHTML = "password is required";
+		return false;
 	}
-	if(conPsw === psw) {
-		document.getElementById("sUpConfirmPwError").innerHTML = "password match";
-		document.getElementById("sUpConfirmPwError").style.color = "green";
-	} else {
+	if(!(conPsw === psw)) {
 		document.getElementById("sUpConfirmPwError").innerHTML = "password doesn't match";
 		document.getElementById("sUpConfirmPwError").style.color = "red";
+		return false;
 	}
+	return true;
 };
 
