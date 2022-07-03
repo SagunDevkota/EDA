178f6a486d90167960a315b68e1919b96e57d4d8
SagunDevkota <sagundevyt@gmail.com>
2022-07-03 12:23:23 +0545

    Mapping for authincated related urls configured

---
+++ src/main/java/eda/controller/AuthController.java
@@ -0,0 +1,33 @@
+package eda.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import eda.dao.UserInfoDAOImpl;
+import eda.dto.UserInfo;
+
+@Controller
+public class AuthController {
+	@Autowired
+	UserInfoDAOImpl userInfoDAOImpl;
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/process-signup")
+	public String processSignup(@ModelAttribute UserInfo userInfo) {
+		System.out.println(userInfo.toString());
+		try {
+			userInfoDAOImpl.saveUserInfo(userInfo);
+		}catch(DuplicateKeyException e) {
+			return "redirect:auth?error=User already exists";
+		}
+		return "redirect:auth?success=true";
+	}
+}
