e31dadc6772f2e6b0191ce0267e0e65c3ae1e930
SagunDevkota <sagundevyt@gmail.com>
2022-07-05 16:27:45 +0545

    No effective changes

--- src/main/java/eda/dao/UserDAOImpl.java
+++ src/main/java/eda/dao/UserDAOImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import eda.dto.User;
 
@@ -16,15 +17,15 @@ public class UserDAOImpl implements UserDAO {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
+	
 	@Override
 	public User findUserByUsername(String name) {
 		//Search user in database
 		String query = "SELECT * FROM Users WHERE email = ?";
 		Object[] args = new Object[] {name};
 		int[] argsType = new int[] {Types.VARCHAR};
-		List<User> users = jdbcTemplate.query(query, args,argsType,new BeanPropertyRowMapper<User>(User.class));
+		List<User> users = jdbcTemplate.query(query, args,argsType,new UserRowMapperImpl());
 		return users.size()>0?users.get(0):null;
-		
 	}
 
 }
