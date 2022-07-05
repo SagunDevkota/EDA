023170f479553cdb82eceff984327190500eef67
SagunDevkota <sagundevyt@gmail.com>
2022-07-05 16:26:03 +0545

    C:/Program Files/Git/ and /dashboard separeated with session attributes

--- src/main/java/eda/controller/HomeController.java
+++ src/main/java/eda/controller/HomeController.java
@@ -1,18 +1,33 @@
 package eda.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import eda.dto.User;
 
 @Controller
+@SessionAttributes("id")
 public class HomeController {
-	@RequestMapping({"/","/dashboard"})
+	@Autowired
+	User user;
+	
+	@RequestMapping("/")
 	public String home() {
 		return "dashboard";
 	}
 	
-	
+	@RequestMapping("/dashboard")
+	public String dashboard(HttpSession session,Model model) {
+		session.setAttribute("id", user.getId());
+		return "dashboard";
+	}
 	
 	@RequestMapping("/user/{name}")
 	public String user(@PathVariable("name") String name,Model model) {
