9987b3d0905f3987fc3279dbdf0fdf1ac14c5f92
SagunDevkota <sagundevyt@gmail.com>
2022-07-08 13:48:46 +0545

    Logic to upload data to database including protecting invalid format and dublicate uploads

--- src/main/java/eda/controller/FileController.java
+++ src/main/java/eda/controller/FileController.java
@@ -8,22 +8,19 @@ import java.util.HashMap;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.google.gson.Gson;
 
 import eda.dao.DataDAOImpl;
+import eda.dto.Data;
 import eda.dto.UploadedFile;
 import eda.report.report.ReportInitiator;
-import eda.dto.User;
 
 @Controller
 public class FileController {
@@ -42,43 +39,57 @@ public class FileController {
 		model.addAttribute("uploadedFile", new UploadedFile());
 		return "temp-upload";
 	}
-	@ResponseBody
+
 	@RequestMapping(value = "/uploadFile",method = RequestMethod.POST)
 	public String uploadCSV(@RequestParam("file") CommonsMultipartFile file,HttpSession session) {
-		System.out.println(file.getName());
-		System.out.println(file.getSize());
-		System.out.println(file.getContentType());
-		System.out.println(file.getOriginalFilename());
-		System.out.println(file.getStorageDescription());
+		if(!file.getContentType().equals("text/csv")) {
+			return "redirect:upload?error=Invalid File Format";
+		}
 		byte[] bytes = file.getBytes();
-		String path = session.getServletContext().getRealPath("/WEB-INF/resources/csv/")+file.getOriginalFilename();
-		FileOutputStream fos = null;
-		try {
-			fos = new FileOutputStream(path);
-			fos.write(bytes);
-			System.out.println("file uploaded at "+path);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
+		String dataString = new String(bytes);
+		String path = session.getServletContext().getRealPath("/WEB-INF/resources/csv/")+dataString.hashCode()+".csv";
+		
+		Data data = new Data();
+		data.setFileName(file.getOriginalFilename());
+		data.setFileSize(file.getSize());
+		data.setFileUrl(path);
+		data.setHash(dataString.hashCode());
+		data.setOwnerId((int)session.getAttribute("id"));
+		
+		Data response = dataDAOImpl.getData(dataString.hashCode(),(int)session.getAttribute("id") );
+		if(response == null) {
+			FileOutputStream fos = null;
 			try {
-				fos.close();
+				fos = new FileOutputStream(path);
+				fos.write(bytes);
+				System.out.println("file uploaded at "+path);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
+			dataDAOImpl.saveData(data);
+			System.out.println("New file uploaded");
+		}else {
+			System.out.println("New file not uploaded");
 		}
-		return "done";
+		return "UploadResponse";
 	}
 	
 	@RequestMapping("/report")
 	public String showReport(@RequestParam("name") String fileName,HttpSession session,Model model) {
 		System.out.println(fileName);
 		HashMap<String, Object> report = new HashMap<>();
-		System.out.println(dataDAOImpl.contains(12345)+" "+session.getAttribute("id"));
+		System.out.println(dataDAOImpl.getData(12345,3)+" "+session.getAttribute("id"));
 		try {
 			report = reportInitiator.getReport(session.getServletContext().getRealPath("/WEB-INF/resources/csv/")+fileName);
 		} catch (IOException e) {
