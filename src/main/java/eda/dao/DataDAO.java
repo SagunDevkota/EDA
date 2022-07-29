52a57efe6b0f7a47ef68a8ae5a9ee8c853d1c4a1
SagunDevkota <sagundevyt@gmail.com>
2022-07-29 10:46:24 +0545

    Changed argument type of hash to string in getData method

--- src/main/java/eda/dao/DataDAO.java
+++ src/main/java/eda/dao/DataDAO.java
@@ -3,7 +3,7 @@ package eda.dao;
 import eda.dto.Data;
 
 public interface DataDAO {
-	public Data getData(long hash,int owner);
+	public Data getData(String hash,int owner);
 	public void saveData(Data data);
 	public boolean shareData(int id);
 	public boolean removeData(int id);
