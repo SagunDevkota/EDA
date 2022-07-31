9cfd44971e59fe77d64a332ae586dd2588bd37fa
Abhishek <abhishek.adhikari276@gmail.com>
2022-07-31 20:17:51 +0545

    add drop down to profile icon

--- src/main/webapp/WEB-INF/resources/js/dashScript.js
+++ src/main/webapp/WEB-INF/resources/js/dashScript.js
@@ -5,3 +5,11 @@ document.querySelector('#search-btn').onclick = () =>{
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
+
+let profileItem = document.querySelector('.profile-items-container');
+
+document.querySelector('#profile-btn').onclick = () =>{
+    profileItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
