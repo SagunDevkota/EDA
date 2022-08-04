1b84fdfa51799e017e6406de6a2124b4dbb394a6
SagunDevkota <sagundevyt@gmail.com>
2022-08-04 18:38:19 +0545

    Overridden getLocalizedMessage

--- src/main/java/eda/exception/DuplicateFileException.java
+++ src/main/java/eda/exception/DuplicateFileException.java
@@ -4,4 +4,10 @@ public class DuplicateFileException extends RuntimeException{
 	public DuplicateFileException(String message) {
 		super(message);
 	}
+	
+	@Override
+	public String getLocalizedMessage() {
+		// TODO Auto-generated method stub
+		return super.getLocalizedMessage();
+	}
 }
