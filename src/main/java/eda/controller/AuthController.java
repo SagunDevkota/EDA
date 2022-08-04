157ab83211309e863f145e866dffe54712ec1e47
SagunDevkota <sagundevyt@gmail.com>
2022-08-04 18:37:04 +0545

    Added login error mapping

--- src/main/java/eda/controller/AuthController.java
+++ src/main/java/eda/controller/AuthController.java
@@ -5,6 +5,9 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
 import eda.dao.UserInfoDAOImpl;
 import eda.dto.UserInfo;
 
@@ -19,13 +22,25 @@ public class AuthController {
 	}
 	
 	@RequestMapping("/process-signup")
-	public String processSignup(@ModelAttribute UserInfo userInfo) {
+	public RedirectView processSignup(@ModelAttribute UserInfo userInfo,RedirectAttributes redir) {
 		System.out.println(userInfo.toString());
+		RedirectView redirectView = new RedirectView("/login", true);
 		try {
 			userInfoDAOImpl.saveUserInfo(userInfo);
 		}catch(DuplicateKeyException e) {
-			return "redirect:login?error=User already exists";
+			redir.addFlashAttribute("error", "User already exists");
+			redir.addFlashAttribute("success", false);
+			return redirectView;
 		}
-		return "redirect:login?success=true";
+		redir.addFlashAttribute("success", true);
+		return redirectView;
+	}
+	
+	@RequestMapping("/loginError")
+	public RedirectView loginError(RedirectAttributes redir) {
+		RedirectView redirectView = new RedirectView("/login", true);
+		redir.addFlashAttribute("error", "Invalid Credentials");
+		redir.addFlashAttribute("success", false);
+		return redirectView;
 	}
 }
