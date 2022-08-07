3ca40d936c147ecfd4076d4cac6178f1ff64a2e0
SagunDevkota <sagundevyt@gmail.com>
2022-08-07 10:37:02 +0545

    profile mapping added

--- src/main/java/eda/controller/HomeController.java
+++ src/main/java/eda/controller/HomeController.java
@@ -11,12 +11,16 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import eda.dto.User;
+import eda.service.UserProfileService;
 
 @Controller
 @SessionAttributes("id")
 public class HomeController {
 	@Autowired
 	User user;
+	
+	@Autowired
+	UserProfileService userProfileService;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -38,5 +42,12 @@ public class HomeController {
 	@RequestMapping("/auth")
 	public String auth() {
 		return "auth";
+	}
+	
+	@RequestMapping("/profile")
+	public String profile(HttpSession session,Model model) {
+		User user2 = userProfileService.getUser((int)session.getAttribute("id"));
+		model.addAttribute("user", user2);
+		return "profile";
 	}
 }
