d196cfbed9a0f4dc11e61ca9f3c1b3a801eb5cb5
SagunDevkota <sagundevyt@gmail.com>
2022-09-05 22:45:51 +0545

    Testing code reverted

--- src/main/java/eda/controller/HomeController.java
+++ src/main/java/eda/controller/HomeController.java
@@ -3,14 +3,22 @@ package eda.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import eda.dto.User;
+import eda.service.EmailSenderService;
 import eda.service.UserProfileService;
 
 @Controller
@@ -21,6 +29,9 @@ public class HomeController {
 	
 	@Autowired
 	UserProfileService userProfileService;
+	
+	@Autowired
+	JavaMailSenderImpl javaMailSenderImpl;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -38,7 +49,7 @@ public class HomeController {
 		model.addAttribute("name", name);
 		return "user";
 	}
-	
+		
 	@RequestMapping("/auth")
 	public String auth() {
 		return "auth";
