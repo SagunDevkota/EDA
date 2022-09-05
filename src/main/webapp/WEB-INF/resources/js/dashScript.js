5472a7daacd3fc97b7d8d818da870d9ef766efc5
SagunDevkota <sagundevyt@gmail.com>
2022-09-05 22:48:30 +0545

    Added xhr request to send contact form info

--- src/main/webapp/WEB-INF/resources/js/dashScript.js
+++ src/main/webapp/WEB-INF/resources/js/dashScript.js
@@ -20,3 +20,35 @@ function togglePopup() {
   .classList.toggle("active");
   toggle = false;
 }
+
+let form = document.querySelector("#supportForm");
+        form.addEventListener("submit", e => {
+            e.preventDefault();
+            var xhr=new XMLHttpRequest();
+            formdata = new FormData(form);
+            var doc = document.getElementById("supportForm");
+            document.getElementById("btn-submit-contact").value = "Sending...";
+            var csrfToken = doc.childNodes[doc.childNodes.length-1].
+            getElementsByTagName("input")[0].value
+            xhr.open("POST","./sendEmail?_csrf="+csrfToken,true);
+            xhr.send(formdata);
+            xhr.onload = function() {
+                console.log(`Loaded: ${xhr.status} ${xhr.response}`);
+                var popup = document.getElementsByClassName("popup")[0];
+                if(popup.classList.length == 1){
+                	popup.classList.add("active");
+                }
+                var x = `${xhr.response}`
+                if(`${xhr.response}` === "Could not send email"){
+                	popup.innerHTML = "<div class='content' style='background: red;'><span class='message'>"+`${xhr.response}`+"</span><span class='close-btn' onclick='togglePopup()'>x</span>"
+                }else{
+                	popup.innerHTML = "<div class='content' style='background: green;'><span class='message'>"+`${xhr.response}`+"</span><span class='close-btn' onclick='togglePopup()'>x</span>"
+                }
+                document.getElementById("btn-submit-contact").value = "contact now";
+                //var response = document.getElementById("responseText")
+                //response.innerHTML = xhr.response
+                // var object = document.getElementsByClassName("response")[0]
+                // object.innerHTML = xhr.response
+                // document.getElementsByClassName("recordingInitialized")[0].innerHTML = ""
+            };
+        })
