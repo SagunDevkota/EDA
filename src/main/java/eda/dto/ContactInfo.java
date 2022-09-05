80b38bc437d1a22bdac22f6f9c23c6cc3c75ba44
SagunDevkota <sagundevyt@gmail.com>
2022-09-05 22:50:00 +0545

    Model class for contact info

---
+++ src/main/java/eda/dto/ContactInfo.java
@@ -0,0 +1,39 @@
+package eda.dto;
+
+public class ContactInfo {
+	private String name;
+	private String message;
+	private String phoneNo;
+	private String email;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "ContactInfo [name=" + name + ", message=" + message + ", phoneNo=" + phoneNo + ", email=" + email + "]";
+	}
+	
+	
+}
